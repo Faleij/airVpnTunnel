c7e81d39939c871ff69702c2c548143e02252c17
Faleij <faleij@live.se>
2016-11-28 13:21:14 +0100

    Added defaults to config

--- bin/config.js
+++ bin/config.js
@@ -4,13 +4,13 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-
 const path = require('path');
-console.log(path.resolve(__dirname, '../config.json'));
+const confPath = path.resolve(__dirname, '../config.json');
+const defaultConfig = require(confPath);
 
 inquirer.prompt([
-    { type: 'confirm', name: 'proxy', message: 'Start VPN Proxy when VPN connection is made?', default: true },
-    { type: 'input', name: 'proxyPort', message: 'What port should the VPN Proxy listen on?', default: 8080, when(answers) { return answers.proxy; }, validate(n) { return Number.isFinite(parseInt(n, 10)); } },
+    { type: 'confirm', name: 'proxy', message: 'Start VPN Proxy when VPN connection is made?', default: defaultConfig.proxy },
+    { type: 'input', name: 'proxyPort', message: 'What port should the VPN Proxy listen on?', default: defaultConfig.proxyPort, when(answers) { return answers.proxy; }, validate(n) { return Number.isFinite(parseInt(n, 10)); } },
 ]).then(function (answers) {
     console.log('\nYou can run this config again with "airVpnTunnel"');
   fs.writeFileSync(path.resolve(__dirname, '../config.json'), JSON.stringify(answers, null, '  '));
